19dfea63f1d01b5a432f925799a227025e7c4534
kazuki sumi <kazuki.sumi@techacademy.jp>
2018-04-24 03:12:39 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
     #Tasklistモデルのレコード(横列)の一覧表示
     def index
        @tasks = Task.all
@@ -21,15 +22,14 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
+        
     end
     
     def show
-        @task = Task.find(params[:id])
+        
     end
     
     def update
-        @task = Task.find(params[:id])
         
             if @task.update(task_params)
                 flash[:success] = 'Taskは正常に更新されました'
@@ -41,7 +41,6 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'Taskは正常に削除されました'
@@ -52,8 +51,13 @@ class TasksController < ApplicationController
     private
    
    #Strong Parameter 
+   
+   def set_task
+       @task = Task.find(params[:id])
+   end
+   
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
     
 end
