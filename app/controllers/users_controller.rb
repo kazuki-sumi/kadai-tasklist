d0505c0bb66b536c6d8410b24683d918b680a7eb
kazuki sumi <kazuki.sumi@techacademy.jp>
2018-06-27 04:15:39 +0000

    ログイン認証と一対多2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:show]
+  
   def index
     @users = User.all.page(params[:page])
   end
@@ -32,4 +34,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+    
 end
